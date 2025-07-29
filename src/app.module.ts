b8dcbcb16208ac98c16e710b6f4ac76246fefd1b
Eden Konde <edenppn@gmail.com>
2025-07-28 23:35:35 -0300

    Configura o projeto e adiciona module e entity postagem

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { PostagemModule } from './postagem/postagem.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Postagem],
+      synchronize: true,
+      logging: true,
+    }),
+    PostagemModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
